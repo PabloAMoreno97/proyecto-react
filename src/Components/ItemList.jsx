a02767840e8dc5ee56344537da77b78bde8328a0
Pablo Moreno <pablolm97@gmail.com>
2022-07-04 18:50:27 -0500

    adding items modules

---
+++ src/Components/ItemList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Item from './Item';
+const ItemList = () => {
+    
+    return (
+        <Item />
+    );
+}
+export default ItemList
--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ItemCount from './ItemCount';
+import ItemList from './ItemList';
 
 const ItemListContainer = ({greeting}) => {
     return (
         <>
             <h1>{greeting}</h1>
+            <ItemList />
             <ItemCount stock={5} />
         </>
     );
